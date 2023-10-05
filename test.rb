1dcea9badfc40db5797e6ed8fb10e38964104f31
Ochiengsteven <stevenochieng6305@gmail.com>
2023-10-05 11:47:09 +0300

    ADD THE TEST FILE

---
+++ test.rb
@@ -0,0 +1,35 @@
+# test.rb
+
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+
+# Create a Person instance
+person = Person.new(1, 20, 'John', true)
+puts 'Person Information:'
+puts "ID: #{person.id}"
+puts "Name: #{person.name}"
+puts "Age: #{person.age}"
+puts "Can use services? #{person.can_use_services?}"
+puts ''
+
+# Create a Student instance
+student = Student.new(2, 16, 'Class A', 'Alice', false)
+puts 'Student Information:'
+puts "ID: #{student.id}"
+puts "Name: #{student.name}"
+puts "Age: #{student.age}"
+puts "Classroom: #{student.classroom}"
+puts "Can play hooky? #{student.play_hooky}"
+puts "Can use services? #{student.can_use_services?}"
+puts ''
+
+# Create a Teacher instance
+teacher = Teacher.new(3, 30, 'Math', 'Mr. Smith', true)
+puts 'Teacher Information:'
+puts "ID: #{teacher.id}"
+puts "Name: #{teacher.name}"
+puts "Age: #{teacher.age}"
+puts "Specialization: #{teacher.specialization}"
+puts "Can use services? #{teacher.can_use_services?}"
+puts ''
