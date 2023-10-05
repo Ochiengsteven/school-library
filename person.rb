ca12e4fa91e1b737eeceab50e92dcaff134df1ca
Ochiengsteven <stevenochieng6305@gmail.com>
2023-10-05 12:59:29 +0300

    ADD PERSONS CORRECTIONS

--- person.rb
+++ person.rb
@@ -11,15 +11,15 @@ class Person
     @parent_permission = parent_permission
   end
 
-  private
-
-  def of_age?
-    @age >= 18
-  end
-
   public
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
